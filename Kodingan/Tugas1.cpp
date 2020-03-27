d64e34943f1c2c5a28cf1f1d7d2ca4d460df4d09
mochammadfarkhan <55824712+mochammadfarkhan@users.noreply.github.com>
2020-03-27 13:24:51 +0700

    Update Tugas1.cpp
    menambah baris kode untuk pertama kalinya

--- Kodingan/Tugas1.cpp
+++ Kodingan/Tugas1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#define max 6
+using namespace std;
 
+main() {
+
+  char pensit[max] = {'p', 'e', 'n', 's', 'i', 't'};
+  char temp;
+  // memasukan isi array dengan urutan terbalik
+  for (int j = 0, i = 1; j < max/2; j++, i++) {
+    temp = pensit[j];
+    pensit[j] = pensit[max - i];
+    pensit[max - i] = temp;
+  }
+
+  // mencetak isi array 
+  for (int i = 0; i < 6; i++) {
+    cout << pensit[i];
+  }
+}
